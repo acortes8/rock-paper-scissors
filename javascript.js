9efd96c7dc86b2858bdafca304c45db93570791f
acortes8 <46253800+acortes8@users.noreply.github.com>
2024-08-01 21:31:35 -0500

    Add logic to play rounds and play full 5 rounds
    Game is complete for current stage

--- javascript.js
+++ javascript.js
@@ -1,3 +1,71 @@
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let index = 0; index < 5; index++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log(`Score: ${humanScore} to ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("You Won!");
+    } else if (computerScore > humanScore) {
+        console.log("You Lost!");
+    } else {
+        console.log("You Tied!");
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You tie! Rock ties Rock");
+                    break;
+                case "scissors":
+                    humanScore++;
+                    console.log("You win! Rock beats Scissors");
+                    break;
+                case "paper":
+                    computerScore++
+                    console.log("You lose! Paper beats Rock");
+                    break;
+            }
+        } else if (humanChoice === "paper") {
+            switch (computerChoice) {
+                case "rock":
+                    humanScore++;
+                    console.log("You win! Paper beats Rock");
+                    break;
+                case "scissors":
+                    computerScore++;
+                    console.log("You lose! Scissors beats Paper");
+                    break;
+                case "paper":
+                    console.log("You tie! Paper ties Paper");
+                    break;
+            }
+        } else if (humanChoice === "scissors") {
+            switch (computerChoice) {
+                case "rock":
+                    computerScore++;
+                    console.log("You lose! Rock beats scissors");
+                    break;
+                case "scissors":
+                    console.log("You tie! Scissors ties Scissors");
+                    break;
+                case "paper":
+                    humanScore++
+                    console.log("You win! Scissors beats Paper");
+                    break;
+            }
+        } else {
+            console.log("invalid round");
+        }
+    }
+}
+
 function getComputerChoice() {
     choice = generateRandomNumber(3);
     if (choice === 0) {
@@ -11,18 +79,17 @@ function getComputerChoice() {
     }
 }
 
-function generateRandomNumber(max = 3) {
-    return Math.floor(Math.random() * max);
-}
-
 function getHumanChoice() {
     choice = window.prompt("Choose your move:\nRock, Paper, or Scissors?");
     return standardizeHumanChoice(choice);
+}
+
+function generateRandomNumber(max = 3) {
+    return Math.floor(Math.random() * max);
 }
 
 function standardizeHumanChoice(choice) {
     return choice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+playGame();
